9434d24818e136a7e34fccef5be2a476e3d49632
AndrewGithinji <andrewgithinji@hotmail.com>
2024-02-23 17:14:24 +0300

    Deleted

--- SAVANNAH_CHALLENGE_2/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('SAVANNAH_CHALLENGE_2')),
-]
